ef4aac8a6d673653712a67d7df01ae43823c2e5c
Pieter J. Fourie <fourie@ivt.baug.ethz.ch>
2023-03-24 15:41:14 +0900

    shortest path alternative based on subpopulation and optional randomWalkProb person attribute

--- src/main/java/org/matsim/router/TouristRoutingModule.java
+++ src/main/java/org/matsim/router/TouristRoutingModule.java
@@ -3,20 +3,29 @@ package org.matsim.router;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.api.core.v01.population.PopulationFactory;
 import org.matsim.core.gbl.Gbl;
+import org.matsim.core.gbl.MatsimRandom;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.core.population.routes.RouteUtils;
 import org.matsim.core.router.RoutingModule;
-import org.matsim.core.router.util.*;
+import org.matsim.core.router.costcalculators.TravelDisutilityFactory;
+import org.matsim.core.router.speedy.SpeedyALTFactory;
+import org.matsim.core.router.util.LeastCostPathCalculator;
+import org.matsim.core.router.util.TouristPathSimulatortest;
+import org.matsim.core.router.util.TravelDisutility;
+import org.matsim.core.router.util.TravelTime;
 import org.matsim.facilities.Facility;
+import org.matsim.vehicles.Vehicle;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class TouristRoutingModule implements RoutingModule {
 
@@ -42,13 +51,34 @@ public class TouristRoutingModule implements RoutingModule {
             Link fromLink = network.getLinks().get(fromFacility.getLinkId());
             Link toLink = network.getLinks().get(toFacility.getLinkId());
 
-            LeastCostPathCalculator.Path path = pathSimulator.simulatePath(fromLink, toLink, departureTime, person, null);
+            Random random = MatsimRandom.getRandom();
+            double chance = random.nextDouble();
+            boolean randomWalker = true;
+            if (person.getAttributes().getAttribute("subpopulation").equals("loc")) {
+                double randomWalkProb = 1.0;
+                try {
+                    randomWalkProb = (double) person.getAttributes().getAttribute("randomWalkProb");
+                }catch (Exception e){
+
+                }
+                randomWalker = chance > randomWalkProb;
+            }
+            LeastCostPathCalculator.Path path = null;
+            if (randomWalker)
+                path = pathSimulator.simulatePath(fromLink, toLink, departureTime, person, null);
+            else {
+                DistancePathCalculator alternativeShortestPath = new DistancePathCalculator(network);
+                path = alternativeShortestPath.getPath(fromLink.getToNode(), toLink.getFromNode());
+            }
+
             if (path == null) {
                 throw new RuntimeException("No route found on inverted network from link "
                         + fromFacility.getLinkId() + " to link " + toFacility.getLinkId() + ".");
             }
 
-            NetworkRoute route = this.populationFactory.getRouteFactories().createRoute(NetworkRoute.class, fromFacility.getLinkId(), toFacility.getLinkId());
+
+            NetworkRoute
+                    route = this.populationFactory.getRouteFactories().createRoute(NetworkRoute.class, fromFacility.getLinkId(), toFacility.getLinkId());
             route.setLinkIds(fromFacility.getLinkId(), NetworkUtils.getLinkIds(path.links), toFacility.getLinkId());
             route.setTravelTime(path.travelTime);
             route.setTravelCost(path.travelCost);
@@ -65,8 +95,65 @@ public class TouristRoutingModule implements RoutingModule {
             newLeg.setTravelTime(0);
         }
         newLeg.setDepartureTime(departureTime);
-        return Arrays.asList( newLeg );
+        return Arrays.asList(newLeg);
 
+    }
+
+    /**
+     * Finds the path between two nodes, assuming constant walking speed of 1m/s
+     */
+    private static class DistancePathCalculator {
+        final static ThreadLocal<LeastCostPathCalculator> var = new ThreadLocal<>();
+        final Network network;
+        final LeastCostPathCalculator lcpc;
+
+        // define how the travel disutility is computed:
+
+        DistancePathCalculator(Network network) {
+            this.network = network;
+
+            // Cache this instance for each thread
+            if (var.get() == null) {
+
+                TravelDisutilityFactory disutilityFactory = new ConstantSpeedTravelDisutilityFactory();
+                TravelTime travelTime = new ConstantSpeedTravelTime();
+
+                var.set(new SpeedyALTFactory().createPathCalculator(network, disutilityFactory.createTravelDisutility(travelTime), travelTime));
+            }
+
+            lcpc = var.get();
+        }
+
+        LeastCostPathCalculator.Path getPath(Node fromNode, Node toNode) {
+
+            return lcpc.calcLeastCostPath(fromNode, toNode, 0, null, null);
+        }
+    }
+
+    private static class ConstantSpeedTravelTime implements TravelTime {
+        @Override
+        public double getLinkTravelTime(Link link, double v, Person person, Vehicle vehicle) {
+            return link.getLength();
+        }
+    }
+
+    private static class ConstantSpeedTravelDisutility implements TravelDisutility {
+        @Override
+        public double getLinkTravelDisutility(Link link, double v, Person person, Vehicle vehicle) {
+            return link.getLength();
+        }
+
+        @Override
+        public double getLinkMinimumTravelDisutility(Link link) {
+            return link.getLength();
+        }
+    }
+
+    private static class ConstantSpeedTravelDisutilityFactory implements TravelDisutilityFactory {
+        @Override
+        public TravelDisutility createTravelDisutility(TravelTime travelTime) {
+            return new ConstantSpeedTravelDisutility();
+        }
     }
 
 
